0a32f98bb470480afdb43c4195a4f871a7adff9e
AlpatyevNikita <chococacaococonut@gmail.com>
2023-02-20 13:34:30 +0300

    added storage

--- HW17.playground/Contents.swift
+++ HW17.playground/Contents.swift
@@ -1,4 +1,15 @@
 import Foundation
 
-// MARK: - Empty project
+// MARK: - LIFO storage implementation
 
+protocol EquatableStack {
+    associatedtype Element
+}
+
+struct Stack<Element>: EquatableStack where Element: Equatable {
+    private var storage = [Element]()
+    var isEmpty: Bool { peek() == nil }
+    func peek() -> Element? { storage.last }
+    mutating func push(_ element: Element) { storage.append(element)  }
+    mutating func pop() -> Element? { storage.popLast() }
+}
